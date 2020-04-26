ddae346e0edd8af9e70dedda091c408143c0720a
Alex de Souza <alex.silva@7comm.com.br>
2020-04-26 05:15:09 -0300

    objeto

---
+++ objeto/1_oo.js
@@ -0,0 +1,11 @@
+/* Princípios importantes: */
+
+/**
+ * 1. Abstração.
+ * 2. Encapsulamento.
+ * 3. Herança. (É um?) -> O ser humano é um animal. Ferrari é um carro. Carro é um automóvel.
+ * 4. Composição (Tem um) -> O carro tem um motor. O carro tem portas. O carro tem rádio.
+ * 5. Poliformismo (Múltiplas formas) -> Classe Veículo -> Subclasses: Carro, Moto, Ônibus, Caminhão.
+ * 5. Poliformismo: Classe Produto -> Subclasses: Oculos, Mochila, Sapato.
+*/
+
